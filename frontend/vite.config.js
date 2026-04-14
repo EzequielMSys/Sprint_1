c4497ea81d8e0359fdd8bd27d9b3ddbbf800ae28
Ezequiel <eze10102008@gmail.com>
2026-04-14 11:28:53 -0300

    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'\nimport react from '@vitejs/plugin-react'\n
+import { defineConfig } from 'vite' 
+import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
